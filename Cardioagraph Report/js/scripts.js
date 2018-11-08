2544b17b79c4338e4d412e67d66f86e5d7bd44af
Yakushenko Evgeniy <yakushenko.evgeny@gmail.com>
2018-11-08 09:46:25 +0300

    Add Cardiograph report

---
+++ Cardioagraph Report/js/scripts.js
@@ -0,0 +1,28 @@
+// window.onload = function () {
+//   	document.getElementById('BtnPrinter').onclick = OnPrintClick;
+// };
+
+// function OnPrintClick(){
+// 	// console.log("sefwer");
+// 	// var links = document.querySelector("link[class=bootstrapLink]");
+// 	// console.log(links);
+// 	// for (var i = 0; i < links.length; i++)
+// 	// {
+// 	// 	console.log(links[i]);
+// 	// 	links[i].disabled = true;
+// 	// }
+	
+// 	window.print();
+// };
+//  
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// 	console.log("scroll");
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector("nav.topnav").style.top = "0";
+//   } else {
+//     document.querySelector("nav.topnav").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
