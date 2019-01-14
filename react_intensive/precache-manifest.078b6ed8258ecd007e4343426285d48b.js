f06e116a2993ad996e9e458935c4fbb8d3804822
Yakushenko Evgeniy <yakushenko.evgeny@gmail.com>
2019-01-14 12:30:54 +0300

    try add react

---
+++ react_intensive/precache-manifest.078b6ed8258ecd007e4343426285d48b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "403540e2b56662d35ff8894434f6ff45",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/media/slide-4.403540e2.jpg"
+  },
+  {
+    "revision": "c1158dee1656acad1f1ee0ba942537b4",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/media/slide-3.c1158dee.jpg"
+  },
+  {
+    "revision": "5be56ba7aae171d0ad8afac95eaac723",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/media/slide-2.5be56ba7.jpg"
+  },
+  {
+    "revision": "52491a4a4ded1b1923f2a278660708ff",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/media/slide-1.52491a4a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ee946ecad2a89f4aca6",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/js/runtime~main.7ee946ec.js"
+  },
+  {
+    "revision": "2b455415a2891b179966",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/js/main.2b455415.chunk.js"
+  },
+  {
+    "revision": "3e78c55b7c4bd611847b",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/js/1.3e78c55b.chunk.js"
+  },
+  {
+    "revision": "2b455415a2891b179966",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/static/css/main.bbb16abb.chunk.css"
+  },
+  {
+    "revision": "2fc9f50e26d452cd72a86635f50f29b1",
+    "url": "/YakushenkoES/YakushenkoES.github.io/react_intensive/index.html"
+  }
+];
